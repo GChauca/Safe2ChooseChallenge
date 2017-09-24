5ca5b33016914f1c847fedbc1b22d7a2301539ad
GChauca <din_suzume@hotmail.com>
2017-09-23 20:50:55 -0500

    Step 2. HI-PL greeting

--- Step2/chat.js
+++ Step2/chat.js
@@ -53,6 +53,41 @@
       });
     });
   } 
+
+  else if(locale === "hi") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+    onlineGreeting = "हमारे साथ बात करो";
+    offlineGreeting = 'एक संदेश छोड़ें';
+    $zopim(function() {
+      $zopim.livechat.setGreetings({
+      'online': onlineGreeting,
+      'offline': offlineGreeting
+      });
+    });
+  } 
+
+  else if(locale === "pl") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+    onlineGreeting = "Porozmawiaj z nami";
+    offlineGreeting = 'Zostaw wiadomość';
+    $zopim(function() {
+      $zopim.livechat.setGreetings({
+      'online': onlineGreeting,
+      'offline': offlineGreeting
+      });
+    });
+  } 
+
   else {
     window.$zopim||(function(d,s){var z=$zopim=function(c){
     z._.push(c)},$=z.s=
